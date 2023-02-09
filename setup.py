1ec1d5e0a4cf301c1c93002dd6db03f19fb029af
Katherine Domingo <katherine.domingo@torchbox.com>
2023-02-09 15:27:31 +0800

    Added Python 3.11 to setup.py classifiers

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Intended Audience :: Developers',
         'Topic :: Internet :: WWW/HTTP',
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
