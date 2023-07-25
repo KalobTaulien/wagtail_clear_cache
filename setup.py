d0c8b4f6a174575a3695e842437337a6c48cd99c
Nick Moreton <nick.moreton@torchbox.com>
2023-07-25 10:52:49 +0100

    Squashed Wagtail 4.2 upgrade:
    commit 1ec1d5e0a4cf301c1c93002dd6db03f19fb029af
    Author: Katherine Domingo <katherine.domingo@torchbox.com>
    Date:   Thu Feb 9 15:27:31 2023 +0800
    
        Added Python 3.11 to setup.py classifiers
    
    commit afba16e85f2d23b2e623b67f880a6e162d450574
    Author: Katherine Domingo <katherine.domingo@torchbox.com>
    Date:   Wed Feb 8 16:12:48 2023 +0800
    
        Set lowerbound for Wagtail to 4.1, Removed conditionals
    
    commit 5e9d687735804dbad528a5b5da9609ab767dcbf1
    Author: Katherine Domingo <katherine.domingo@torchbox.com>
    Date:   Fri Oct 28 17:20:03 2022 +0800
    
        Update setup, Include Nick's PR, dropped support for versions of Wagtail lower than 2.15 and Django versions lower than 3.2
    
    commit c16f0c5ef0a1a0a9275a6e81dd8e6889756c6cfe
    Author: Nick Moreton <nickmoreton@me.com>
    Date:   Thu Jul 21 10:00:30 2022 +0100
    
        Update wagtail_clear_cache/wagtail_hooks.py
    
    commit 74c66162eda54571cb00996c49fd4296647b84de
    Author: Nick Moreton <nickmoreton@me.com>
    Date:   Tue Jul 19 13:53:42 2022 +0100
    
        Update wagtail_hooks.py
    
    commit c159d15634fa90ae12724004dc5e32a7ed456a59
    Author: nick.moreton <nick.moreton@torchbox.com>
    Date:   Tue Jul 19 12:40:00 2022 +0100
    
        Wagtail v3 adjustments

--- setup.py
+++ setup.py
@@ -22,21 +22,20 @@ setup(
         'Framework :: Django :: 4.0',
         'Framework :: Django :: 4.1',
         'Framework :: Wagtail',
-        "Framework :: Wagtail :: 2",
-        "Framework :: Wagtail :: 3",
         "Framework :: Wagtail :: 4",
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Intended Audience :: Developers',
         'Topic :: Internet :: WWW/HTTP',
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     keywords='development',
     install_requires=[
-        'wagtail>=2.15',
+        'wagtail>=4.1',
         'Django>=3.2'
     ]
 )
